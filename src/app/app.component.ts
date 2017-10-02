fc996c91738702cf6c18a0db0844c492dd8f20b7
Sema García <jose-manuel.garcia@gft.com>
2017-10-03 01:07:12 +0200

    Add implementation for fullscreen and keep screen awake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AndroidFullScreen } from '@ionic-native/android-full-screen';
+import { Insomnia } from '@ionic-native/insomnia';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,12 +12,28 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private androidFullScreen: AndroidFullScreen, 
+              private insomnia: Insomnia,
+              platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // Full screen
+      this.androidFullScreen.isImmersiveModeSupported()
+        .then(() => this.androidFullScreen.immersiveMode())
+        .catch((error: any) => console.log(error));
+
+      // Prevent the screen of the mobile device from falling asleep
+      this.insomnia.keepAwake()
+        .then(
+          () => console.log('success'),
+          () => console.log('error')
+        );
     });
   }
 }
