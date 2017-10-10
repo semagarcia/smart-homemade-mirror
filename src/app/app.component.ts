8ca12c6dd8f534d31dbf2de2de08910353a62977
kmikodev <jcardonavillegas@gmail.com>
2017-10-11 00:52:22 +0200

    feat(Artyom): Se añade clase de reconocimiento de voz y lector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -6,17 +6,18 @@ import { AndroidFullScreen } from '@ionic-native/android-full-screen';
 import { Insomnia } from '@ionic-native/insomnia';
 
 import { HomePage } from './../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
+export class MyApp implements OnInit {
+  rootPage: any = HomePage;
 
-  constructor(private androidFullScreen: AndroidFullScreen, 
-              private insomnia: Insomnia,
-              platform: Platform, 
-              statusBar: StatusBar, 
-              splashScreen: SplashScreen) {
+  constructor(private androidFullScreen: AndroidFullScreen,
+    private insomnia: Insomnia,
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -31,10 +32,15 @@ export class MyApp {
       // Prevent the screen of the mobile device from falling asleep
       this.insomnia.keepAwake()
         .then(
-          () => console.log('success'),
-          () => console.log('error')
+        () => console.log('success'),
+        () => console.log('error')
         );
     });
   }
+
+  ngOnInit() {
+  
+  }
+
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { App, ViewController } from 'ionic-angular';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { Observable, Subscription } from 'rxjs/Rx';
+import Artyom from './../../app/shared/services/artyom.service';
+
 
 @Component({
   selector: 'page-home',
@@ -17,9 +19,103 @@ export class HomePage implements OnInit, OnDestroy {
     public appCtrl: App, public platform: Platform) { }
 
   ngOnInit() {
+    // Create a variable that stores your instance
+    const artyom = new Artyom();
+
+    // Add command (Short code artisan way)
+    artyom.on(['Hola', 'Good afternoon']).then((i) => {
+      switch (i) {
+        case 0:
+          artyom.say("Hola, how are you?");
+          break;
+        case 1:
+          artyom.say("Good afternoon, how are you?");
+          break;
+      }
+    });
+
+    // Smart command (Short code artisan way), set the second parameter of .on to true
+    artyom.on(['Repeat after me *'], true).then((i, wildcard) => {
+      artyom.say("You've said : " + wildcard);
+    });
+
+    // or add some commandsDemostrations in the normal way
+    artyom.addCommands([
+      {
+        indexes: ['Hello', 'Hi', 'is someone there'],
+        action: (i) => {
+          artyom.say("Hello, it's me");
+        }
+      },
+      {
+        indexes: ['Repeat after me *'],
+        smart: true,
+        action: (i, wildcard) => {
+          artyom.say("You've said : " + wildcard);
+        }
+      },
+      // The smart commands support regular expressions
+      {
+        indexes: [/Good Morning/i],
+        smart: true,
+        action: (i, wildcard) => {
+          artyom.say("You've said : " + wildcard);
+        }
+      },
+      {
+        indexes: ['shut down yourself'],
+        action: (i, wildcard) => {
+          artyom.fatality().then(() => {
+            console.log("Artyom succesfully stopped");
+          });
+        }
+      },
+    ]);
+
+    // Start the commands !
+    artyom.initialize({
+      lang: "en-GB", // GreatBritain english
+      continuous: true, // Listen forever
+      soundex: true,// Use the soundex algorithm to increase accuracy
+      debug: true, // Show messages in the console
+      executionKeyword: "and do it now",
+      listen: true, // Start to listen commands !
+
+      // If providen, you can only trigger a command if you say its name
+      // e.g to trigger Good Morning, you need to say "Jarvis Good Morning"
+      name: "Jarvis"
+    }).then(() => {
+      console.log("Artyom has been succesfully initialized");
+    }).catch((err) => {
+      console.error("Artyom couldn't be initialized: ", err);
+    });
+
     this.timer$ = Observable.timer(0, 1000).subscribe(
       () => this.today = new Date()
     );
+  }
+
+
+  talk() {
+
+    const artyom = new Artyom();
+    /**
+   * To speech text
+   */
+    artyom.say("Hola amigo que tal estas?", {
+      lang: "es-ES",
+      onStart: () => {
+        console.log("Reading ...");
+      },
+      onEnd: () => {
+        /* console.log("No more text to talk");
+ 
+         // Force the language of a single speechSynthesis
+         artyom.say("Hola, esto está en Español", {
+           lang: "es-ES"
+         });*/
+      }
+    });
   }
 
   onExit() {
