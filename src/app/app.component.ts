b303e5765bbdd2203b9c9ed2ba1829f5e57d1c10
jmcn <jmcn@gft.com>
2017-10-11 12:59:15 +0200

    feat(Control voz)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { NeonPage } from './../pages/neon/neon';
+import { Subscription, Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {  Platform } from 'ionic-angular';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AndroidFullScreen } from '@ionic-native/android-full-screen';
@@ -7,11 +10,15 @@ import { Insomnia } from '@ionic-native/insomnia';
 
 import { HomePage } from './../pages/home/home';
 
+import Artyom from './../app/shared/services/artyom.service';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp implements OnInit {
   rootPage: any = HomePage;
+  timer$: Subscription;
+  today;
 
   constructor(private androidFullScreen: AndroidFullScreen,
     private insomnia: Insomnia,
@@ -37,9 +44,82 @@ export class MyApp implements OnInit {
         );
     });
   }
-
   ngOnInit() {
-  
+    // Create a variable that stores your instance
+    const artyom = new Artyom();
+
+    // Add command (Short code artisan way)
+    artyom.on(['prueba', 'termo']).then((i) => {
+      switch (i) {
+        case 0:
+          artyom.say("esto es una prueba");
+          break;
+        case 1:
+          artyom.say("esto es un termo");
+          break;
+      }
+    });
+
+    // // Smart command (Short code artisan way), set the second parameter of .on to true
+    // artyom.on(['Repeat after me *'], true).then((i, wildcard) => {
+    //   artyom.say("You've said : " + wildcard);
+    // });
+
+    // or add some commandsDemostrations in the normal way
+    artyom.addCommands([
+      {
+        indexes: ['Cómo te llamas'],
+        action: (i) => {
+          artyom.say("Me llamo Pepe");
+          
+        }
+      }
+    ]);
+
+    // Start the commands !
+    artyom.initialize({
+      lang: "es-ES", // GreatBritain english
+      continuous: true, // Listen forever
+      soundex: true,// Use the soundex algorithm to increase accuracy
+      debug: true, // Show messages in the console
+      executionKeyword: "empezar",
+      listen: true, // Start to listen commands !
+
+      // If providen, you can only trigger a command if you say its name
+      // e.g to trigger Good Morning, you need to say "Jarvis Good Morning"
+      name: "comando"
+    }).then(() => {
+      console.log("Artyom has been succesfully initialized");
+    }).catch((err) => {
+      console.error("Artyom couldn't be initialized: ", err);
+    });
+
+    this.timer$ = Observable.timer(0, 1000).subscribe(
+      () => this.today = new Date()
+    );
+  }
+
+
+  talk() {
+
+    const artyom = new Artyom();
+    /**
+   * To speech text
+   */
+    artyom.say("Si amigo que tal estas?", {
+      lang: "es-ES",
+      onStart: () => {
+        console.log("Reading ...");
+      },
+      onEnd: () => {
+        /* console.log("No more text to talk");
+ 
+         // Force the language of a single speechSynthesis
+         artyom.say("Si, esto está en Español", {
+           lang: "es-ES"
+         });*/
+      }
+    });
   }
 
 }
