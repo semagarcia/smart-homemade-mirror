a6f4688fcf8104b06d3348d19e946179782e3209
kmikodev <jcardonavillegas@gmail.com>
2017-10-12 01:44:22 +0200

    feat(Directiva, servicio): Se crea directiva para esconder y mostrar los siguientes widgets, un servicio para gestionar el artyom y un comando tell en el que se pueden añadir frases para preguntas

--- src/app/shared/directives/artyom.showHide.directive.ts
+++ src/app/shared/directives/artyom.showHide.directive.ts
@@ -52,7 +52,7 @@ export class ArtyomShowHideDirective implements OnInit {
 
     showHide(i) {
         console.log(this._finalCommand, this._finalCommand[i], i);
-        if (i > 0 && i < this._showLen) {
+        if (i >= 0 && i < this._showLen) {
             this._artyom.say(this._finalCommand[i]);
             if (this.el.nativeElement.getAttribute('hidden') !== null) {
                 this.el.nativeElement.removeAttribute('hidden');
