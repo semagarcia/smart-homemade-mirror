c19e4405e9f96772016f3869c45b3201bbf760d1
kmikodev <jcardonavillegas@gmail.com>
2017-10-12 13:55:04 +0200

    feat(Auido)

--- src/app/shared/directives/artyom.showHide.directive.ts
+++ src/app/shared/directives/artyom.showHide.directive.ts
@@ -7,13 +7,15 @@ import {
 
 import { ArtyomAccessService } from './../services/artyon.access.service';
 
-@Directive({ selector: '[artyomDirective]' })
+@Directive({ selector: '[artyomShowHide]' })
 
 export class ArtyomShowHideDirective implements OnInit {
 
-    @Input('artyomDirective') command: [string];
-    private _show = ["muéstrame", "me muestras", "quiero ver", "enseñame"];
-    private _hidde = ["ocúltame", "quiero dejar de ver", "ya he visto"];
+    @Input('artyomShowHide') command: [string];
+    private _show = ["muéstrame", "me muestras", "quiero ver", "enséñame"];
+    private _showing = ["mostrando", "te muestro", "ahí tienes", "enseñando"];
+    private _hidde = ["ocultame", "quiero dejar de ver", "ya he visto"];
+    private _hiding = ["ocultando", "dejando de ver", "ya no verás"];
     private _showLen: number;
     private _finalCommand: [string];
     constructor(private el: ElementRef,
@@ -52,10 +54,41 @@ export class ArtyomShowHideDirective implements OnInit {
 
     showHide(i) {
         console.log(this._finalCommand, this._finalCommand[i], i);
+
         if (i >= 0 && i < this._showLen) {
-                this.el.nativeElement.removeAttribute('hidden');
+            this.el.nativeElement.removeAttribute('hidden');
         } else {
             this.el.nativeElement.setAttribute('hidden', true);
+        }
+
+        let response: string;
+        this._show.forEach(
+            (item) => {
+
+                if (this.findResponse(item, i, this._show, this._showing)) {
+                    response = this.findResponse(item, i, this._show, this._showing);
+                }
+
+            }
+        );
+
+        this._hidde.forEach(
+            (item) => {
+                if (this.findResponse(item, i, this._hidde, this._hiding)) {
+                    response = this.findResponse(item, i, this._hidde, this._hiding);
+                }
+            }
+        );
+
+        this._artyom.say(response);
+    }
+
+    findResponse(item, i, verb, ing) {
+        const element = this._finalCommand[i];
+        console.debug(`Final Command ${element}`);
+        if (element.indexOf(item) > -1) {
+            return element.replace(item, ing[verb.indexOf(item)]);
+
         }
     }
 }
